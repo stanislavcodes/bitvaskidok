1666d20e76585fe78dc3895e3815425fed0c7527
stanislavcodes <korchevsky2004@gmail.com>
2020-03-24 15:07:36 +0200

    smooth scroll fix

--- js/script.js
+++ js/script.js
@@ -76,70 +76,9 @@ questions[1].addEventListener("click", check(1));
 questions[2].addEventListener("click", check(2));
 
 // smooth scrolling
-
-function smoothScroll(trgt, duration) {
-  let target = document.querySelector(trgt);
-  let targetPosition = target.getBoundingClientRect().top;
-  let startPosition = window.pageYOffset;
-  let distance = targetPosition - startPosition;
-  let startTime = null;
-
-  function animation(currentTime) {
-    if (startTime === null) startTime = currentTime;
-    let TimeElapsed = currentTime - startTime;
-    let run = ease(TimeElapsed, startPosition, distance, duration);
-    window.scrollTo(0, run + 10);
-    if (TimeElapsed < duration) requestAnimationFrame(animation);
-  }
-
-  function ease(t, b, c, d) {
-    t /= d / 2;
-    if (t < 1) return (c / 2) * t * t + b;
-    t--;
-    return (-c / 2) * (t * (t - 2) - 1) + b;
-  }
-
-  requestAnimationFrame(animation);
-}
-
-// navigation
-
-let linkHow = document.querySelector(".linkHow");
-let linkWho = document.querySelector(".linkWho");
-let linkTar = document.querySelector(".linkTar");
-let linkOrg = document.querySelector(".linkOrg");
-let linkCon = document.querySelector(".linkCon");
-
-let linkHow2 = document.querySelector(".linkHow-2");
-let linkWho2 = document.querySelector(".linkWho-2");
-let linkTar2 = document.querySelector(".linkTar-2");
-let linkOrg2 = document.querySelector(".linkOrg-2");
-let linkCon2 = document.querySelector(".linkCon-2");
-
-document.querySelector(".linkHow").addEventListener("click", function() {
-  smoothScroll(".how", 1000);
+var scroll = new SmoothScroll('.nav a[href*="#"]', {
+  speed: 400
 });
-linkHow2.addEventListener("click", function() {
-  smoothScroll(".how", 1000);
-});
-
-linkWho.addEventListener("click", function() {
-  smoothScroll(".who", 1000);
-});
-linkWho2.addEventListener("click", function() {
-  smoothScroll(".who", 1000);
-});
-
-linkTar.addEventListener("click", function() {
-  smoothScroll(".tariffs", 1000);
-});
-linkTar2.addEventListener("click", function() {
-  smoothScroll(".tariffs", 1000);
-});
-
-linkOrg.addEventListener("click", function() {
-  smoothScroll(".organizers", 1000);
-});
-linkOrg2.addEventListener("click", function() {
-  smoothScroll(".organizers", 1000);
+var easeOutQuart = new SmoothScroll('[data-easing="linear"]', {
+  easing: "linear"
 });
