612d5f5f326cb7abace5b9f269db13dd2826392a
stanislavcodes <korchevsky2004@gmail.com>
2020-04-15 20:36:09 +0300

    fix plans and questions on mobile

--- js/script.js
+++ js/script.js
@@ -27,10 +27,8 @@ function navMediaCheck(g) {
     document.addEventListener("scroll", function () {
       if (window.pageYOffset >= 100) {
         nav2.style.backgroundColor = `#ffffff`;
-        console.log("white");
       } else {
         nav2.style.backgroundColor = `#ffd62e`;
-        console.log("yellow");
       }
     });
   }
@@ -110,47 +108,176 @@ var x = window.matchMedia("(min-width: 1029px)");
 parallaxMediaCheck(x);
 
 // creating our arrays
+// const questions = document.querySelectorAll(".question");
+// const answers = document.querySelectorAll(".answer");
+// const icons = document.querySelectorAll(".question__icon");
+// const texts = document.querySelectorAll(".answer__text");
+
+// function defaultState() {
+//   for (i = 0; i < questions.length; i++) {
+//     answers[i].classList.add("answer--closed");
+//     answers[i].style.height = `0`;
+//     answers[i].style.padding = `0 3rem`;
+//     icons[i].style.transform = `rotate(0)`;
+//   }
+// }
+
+// function fn(a) {
+//   defaultState();
+//   answers[a].classList.remove("answer--closed");
+//   answers[a].style.padding = `3rem`;
+//   texts[a].style.display = `block`;
+//   icons[a].style.transform = `rotate(-180deg)`;
+
+//   // calculating height
+//   let height = window.getComputedStyle(texts[a]).height;
+//   answers[a].style.height = `calc(${height} + 6rem)`;
+
+// }
+
+// function check(b) {
+//   questions[b].addEventListener("click", function () {
+//     if (answers[b].classList.contains("answer--closed")) {
+//       fn(b);
+//     } else {
+//       defaultState();
+//     }
+//   });
+// }
+
+// questions[0].addEventListener("click", check(0));
+// questions[1].addEventListener("click", check(1));
+// questions[2].addEventListener("click", check(2));
+
+////////////////////////////////////////////////////////////////////
+// const plans = document.querySelectorAll(".plan");
+const plans = document.querySelectorAll(".plan-box");
+const features = document.querySelectorAll(".feature");
+const planIcons = document.querySelectorAll(".plan__icon");
+const planTexts = document.querySelectorAll(".feature__text");
+const planRows = document.querySelectorAll(".calc-height");
+
 const questions = document.querySelectorAll(".question");
 const answers = document.querySelectorAll(".answer");
-const icons = document.querySelectorAll(".question__icon");
-const texts = document.querySelectorAll(".answer__text");
+const quizIcons = document.querySelectorAll(".question__icon");
+const quizTexts = document.querySelectorAll(".answer__text");
+// const quizTextBoxes = document.querySelectorAll(".answer__text-box");
 
-function defaultState() {
-  for (i = 0; i < questions.length; i++) {
-    answers[i].classList.add("answer--closed");
-    answers[i].style.height = `0`;
-    answers[i].style.padding = `0`;
-    texts[i].style.display = `none`;
-    icons[i].style.transform = `rotate(0)`;
+let planHeights = [];
+let quizHeights = [];
+
+function calcHeight(arrIn, arrOut) {
+  for (let i = 0; i < arrIn.length; i++) {
+    arrOut.push(window.getComputedStyle(arrIn[i]).height);
+  }
+  return arrOut;
+}
+
+calcHeight(planRows, planHeights);
+calcHeight(quizTexts, quizHeights);
+console.log(planHeights);
+console.log(quizHeights);
+
+function defaultState(animateArr, iconsArr, num, paddingTop, arrIn, arrOut) {
+  animateArr[num].style.height = `0`;
+  animateArr[num].classList.add("closed");
+  animateArr[num].style.padding = `0 ${paddingTop}rem`;
+  iconsArr[num].style.transform = `rotate(0)`;
+
+  for (let i = 0; i < animateArr.length; i++) {
+    animateArr[i].style.height = `0`;
+    animateArr[i].classList.add("closed");
+    animateArr[i].style.padding = `0 ${paddingTop}rem`;
+    iconsArr[i].style.transform = `rotate(0)`;
   }
 }
 
-function fn(a) {
-  defaultState();
-  answers[a].classList.remove("answer--closed");
-  answers[a].style.padding = `3rem`;
-  texts[a].style.display = `block`;
+function fn(
+  animateArr,
+  iconsArr,
+  num,
+  arrIn,
+  arrOut,
+  paddingTop,
+  paddingBottom
+) {
+  defaultState(animateArr, iconsArr, num, paddingTop, arrIn, arrOut);
 
-  // calculating height
-  let height = window.getComputedStyle(texts[a]).height;
-  answers[a].style.height = `calc(${height} + 6rem)`;
+  animateArr[num].classList.remove("closed");
+  animateArr[num].style.padding = `3rem ${paddingTop}rem`;
+  iconsArr[num].style.transform = `rotate(-180deg)`;
 
-  icons[a].style.transform = `rotate(-180deg)`;
+  // calcHeight(arrIn, arrOut);
+
+  if (arrOut.length > 3) {
+    animateArr[num].style.height = `calc(
+		${arrOut[0]} + 
+		${arrOut[1]} + 
+		${arrOut[2]} + 
+		${arrOut[3]} + 
+		${arrOut[4]} + 
+    ${arrOut[5]} + 
+    6rem +
+    (${paddingBottom}rem) * 5) `;
+  } else {
+    // console.log(arrOut[num]);
+
+    animateArr[num].style.height = `calc(
+		${arrOut[num]} + 
+    6rem +
+    (${paddingBottom}rem) * 5) `;
+
+    console.log(
+      (animateArr[num].style.height = `calc(
+		${arrOut[num]} + 
+    6rem +
+    (${paddingBottom}rem) * 5) `)
+    );
+  }
 }
 
-function check(b) {
-  questions[b].addEventListener("click", function () {
-    if (answers[b].classList.contains("answer--closed")) {
-      fn(b);
+function check(
+  listenArr,
+  animateArr,
+  iconsArr,
+  num,
+  arrIn,
+  arrOut,
+  paddingTop,
+  paddingBottom
+) {
+  listenArr[num].addEventListener("click", function () {
+    if (animateArr[num].classList.contains("closed")) {
+      fn(animateArr, iconsArr, num, arrIn, arrOut, paddingTop, paddingBottom);
     } else {
-      defaultState();
+      defaultState(animateArr, iconsArr, num, paddingTop, arrIn, arrOut);
     }
   });
 }
 
-questions[0].addEventListener("click", check(0));
-questions[1].addEventListener("click", check(1));
-questions[2].addEventListener("click", check(2));
+(function () {
+  for (let i = 0; i < features.length; i++) {
+    defaultState(features, planIcons, i, 1, planRows, planHeights);
+  }
+  for (let i = 0; i < answers.length; i++) {
+    defaultState(answers, quizIcons, i, 3, quizTexts, quizHeights);
+  }
+})();
+
+for (i = 0; i < 4; i++) {
+  plans[i].addEventListener(
+    "click",
+    check(plans, features, planIcons, i, planRows, planHeights, 1, 2)
+  );
+}
+
+for (i = 0; i < questions.length; i++) {
+  questions[i].addEventListener(
+    "click",
+    check(questions, answers, quizIcons, i, quizTexts, quizHeights, 3, 0)
+  );
+}
+////////////////////////////////////////////////////////////////////
 
 // smooth scrolling
 var scroll = new SmoothScroll('.nav a[href*="#"]', {
@@ -219,3 +346,5 @@ document.addEventListener("scroll", function () {
   } else {
   }
 });
+
+///////////////////////////////////////////
